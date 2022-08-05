f91909e5c4f50ccf146b1b913b174b311a98ff5c
中井　惇平 <junoits@nakai-atsushitairanoMacBook-Air-2.local>
2022-08-05 21:49:28 +0900

    all done

---
+++ main.js
@@ -0,0 +1,63 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const canvas = document.getElementById("canvas");
+// scene
+const scene = new THREE.Scene();
+
+// size
+const sizes = {
+  width: innerWidth,
+  height: innerHeight,
+};
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 3000);
+camera.position.set(0, 500, 1000);
+scene.add(camera);
+
+// renderer
+const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// envimage
+
+const loader = new THREE.CubeTextureLoader();
+scene.background = loader.setPath("Skybox/").load([
+  "left.png",
+  "right.png", 
+  "up.png",
+  "down.png",
+  "front.png",
+  "back.png"
+]);
+
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(500);
+
+// cubeCamera
+const cubeCamera =  new THREE.CubeCamera(1, 1000, cubeRenderTarget);
+scene.add(cubeCamera);
+
+// object
+const material = new THREE.MeshBasicMaterial({
+  envMap: cubeRenderTarget.texture,
+  reflectivity: 1,
+});
+const geometry = new THREE.SphereGeometry(350, 50, 50);
+const sphere = new THREE.Mesh(geometry, material);
+sphere.position.set(0, 100, 0);
+scene.add(sphere);
+
+function animate() {
+  controls.update();
+  cubeCamera.update( renderer, scene );
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(animate);
+}
+
+animate();
